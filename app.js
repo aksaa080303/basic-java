3a4da53b69f389e7947810ae2a416cccbddc99c4
Sahrul Harmawan <161696538+aksaa080303@users.noreply.github.com>
2024-09-27 15:37:25 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// console.log ('Halo Mars')
+
+// var firstName = "Aksa";
+//  function sayHallo () {
+//         var lastName = "Haerm"; // Local Scope
+//  }
+//  console.log("Nama Saya, "+firstName);
+// //  console.log("Nama Saya, "+lastName); // erorr
+
+// kuliah = "Nusa putra University"
+// console.log(kuliah)
+
+// // belajar Var, const dam Let
+
+// var firstName = "aksa" ; // bisa kebaca  secara keseluruham
+// let firstName = "Aksa" ; // hanya terbaca dari block scope
+// const firstName = "Aksa"; // Tidak bisa berubah nama 
+
+// var sisi = prompt("Masukan sisi");
+// var luas = sisi*sisi;
+
+// console.log("luas persegi adalah "+ luas);
+
+var tahun = prompt("Masukan Umur Lahir");
+var umur = 2024 - tahun;
+console.log("Umur skrng adalah" + umur); 
+
+//menghitung persegi panjang
+// var panjang = prompt("masukan panjang");
+// var lebar = prompt("masukan lebar");
+// var luas =panjang*lebar;
+// console.log ("luas persegi panjang adalah" +panjang*lebar);
