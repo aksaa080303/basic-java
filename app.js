77cba3a83b02bfdda3e112de27c9dce19c2232d1
Sahrul Harmawan <161696538+aksaa080303@users.noreply.github.com>
2024-10-04 07:46:25 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
-// console.log ('Halo Mars')
+// keliling lingkaran
+const jari = prompt("masukan jari jari :")
+const keliling =2*Math.PI*jari
+console.log(`keliling lingkaran adalah${keliling}`)
 
-// var firstName = "Aksa";
-//  function sayHallo () {
-//         var lastName = "Haerm"; // Local Scope
-//  }
-//  console.log("Nama Saya, "+firstName);
-// //  console.log("Nama Saya, "+lastName); // erorr
+// luas persegi panjang
+const panjang = prompt("masukan panjang :");
+const lebar = prompt("masukan lebar :");
+const luas =  panjang*lebar;
+console.log(`luas persegi panjang adalah ${luas}`)
 
-// kuliah = "Nusa putra University"
-// console.log(kuliah)
+//volume kubus
+const sisi = prompt("masukan sisi :")
+const volume = sisi*sisi*sisi;
+console.log(`volume kubus adalah ${volume}`)
 
-// // belajar Var, const dam Let
+// menghitung suhu
+const suhu = prompt("masukan suhu :")
+const celcius = (suhu*(9/5))+32;
+console.log(celcius)
 
-// var firstName = "aksa" ; // bisa kebaca  secara keseluruham
-// let firstName = "Aksa" ; // hanya terbaca dari block scope
-// const firstName = "Aksa"; // Tidak bisa berubah nama 
+// menghitung harga telur  
+const telurperkg = 28000;
+const totalbeli = prompt("masukan total beli telur :")
+const totalharga = telurperkg * totalbeli
+const uangbayar = prompt("masukan uang bayar :")
+const kembalian = uangbayar - totalharga
+console.log(kembalian)
 
-// var sisi = prompt("Masukan sisi");
-// var luas = sisi*sisi;
 
-// console.log("luas persegi adalah "+ luas);
 
-var tahun = prompt("Masukan Umur Lahir");
-var umur = 2024 - tahun;
-console.log("Umur skrng adalah" + umur); 
 
-//menghitung persegi panjang
-// var panjang = prompt("masukan panjang");
-// var lebar = prompt("masukan lebar");
-// var luas =panjang*lebar;
-// console.log ("luas persegi panjang adalah" +panjang*lebar);
+
